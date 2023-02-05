64f5c92d08bfd6d50fe6ddeeb9ad2a58eb78d167
Kan <kanyakancat@gmail.com>
2023-02-05 11:12:19 +0300

    added semantik & tab-move

--- js/burger.js
+++ js/burger.js
@@ -3,6 +3,12 @@ window.addEventListener("DOMContentLoaded", function () {
     .querySelector("#nav_toggle")
     .addEventListener("click", function () {
       document.querySelector(".header__burger").classList.toggle("active");
+      document.querySelector(".burger-nav").classList.toggle("active");
+
+      // setTimeout(function () {
+      //   document.querySelector(".header__burger").classList.toggle("transform");
+      // }, 10);
+
       $(this).toggleClass("active"); //для черточек/крестика
     });
 });
